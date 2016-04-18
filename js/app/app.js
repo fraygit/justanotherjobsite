9baa2aea522cec34eb3fbccedd850a1bd101334b
fray <francis.yanga@gmail.com>
2016-04-19 00:14:56 +1200

    initial

---
+++ js/app/app.js
@@ -0,0 +1,31 @@
+﻿var jajsApp = angular.module('jajsApp', ['ui.router']);
+
+jajsApp.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+        .state('home', {
+            url: '',
+            abstract: true,
+            views: {
+                'header':
+                    {
+                        templateUrl: 'js/app/partials/header.html'
+                    },
+                'footer':
+                    {
+                        templateUrl: 'js/app/partials/footer.html'
+                    }
+            }
+        })
+
+        .state('home.index', {
+            url: '/home',
+            views:{
+                'container@': {
+                    templateUrl: 'js/app/templates/home.html'
+                }
+            }
+            
+        })
+});
