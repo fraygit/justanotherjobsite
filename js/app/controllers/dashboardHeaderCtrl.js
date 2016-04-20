82eee0d27d7128533f72d240eaae299002d84e2c
fray <francis.yanga@gmail.com>
2016-04-20 23:03:23 +1200

    dashboard active menu header class

--- js/app/controllers/dashboardHeaderCtrl.js
+++ js/app/controllers/dashboardHeaderCtrl.js
@@ -1,6 +1,13 @@
 ﻿
 angular.module('jajsApp').controller('DashboardHeaderController', ['$scope', '$http', 'UserService', '$state', '$rootScope', function ($scope, $http, UserService, $state, $rootScope) {
 
+    $scope.currentState = $state.current.name;
+
+    $rootScope.$on('$stateChangeStart',
+    function (event, toState, toParams, fromState, fromParams) {
+        $scope.currentState = toState.name;
+    })
+
     $rootScope.$on('dashboard', function () {
         $scope.isAuthenticated = true;
     });
@@ -8,5 +15,12 @@ angular.module('jajsApp').controller('DashboardHeaderController', ['$scope', '$h
     $rootScope.$on('home', function () {
         $scope.isAuthenticated = false;
     });
+
+    if ($state.current.name.indexOf('.dashboard') > -1) {
+        $rootScope.$broadcast('dashboard');
+    }
+    else {
+        $rootScope.$broadcast('home');
+    }
 
 }]);
